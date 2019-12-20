2f6388450a2a867343bef8e1a80c2188a20b0148
Artjom Pahhomov <arpahh@ttu.ee>
2019-12-20 19:47:03 +0200

    Exam template done

---
+++ Exam/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        
+        public AppDbContext(DbContextOptions options) : base(options) {}
+    }
+}
---
+++ Exam/Domain/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Person
+    {
+        [Key]
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
